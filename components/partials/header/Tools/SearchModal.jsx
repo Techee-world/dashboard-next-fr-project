d1f903fed169bbe9071918fe3ad782ce6e67e63b
muhammed navas <muhammednavas6287@gmail.com>
2024-03-31 23:39:14 +0530

    create tages base search bar ,add search suggestion no adding close button and bg #1

--- components/partials/header/Tools/SearchModal.jsx
+++ components/partials/header/Tools/SearchModal.jsx
@@ -1,57 +1,54 @@
 import React, { Fragment, useState } from "react";
 import { Dialog, Transition, Combobox } from "@headlessui/react";
 import Icon from "@/components/ui/Icon";
+
 const SearchModal = () => {
-  let [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [query, setQuery] = useState("");
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [backgroundColor, setBackgroundColor] = useState(" ");
+  const [sample , setSample ] = useState()
+  const [sample1 , setSample1 ] = useState()
 
-  function closeModal() {
-    setIsOpen(false);
-  }
+  const searchList = {
+    kerala: ["a", "b"],
+    delhi: ["10", "20"],
+    tamilnad: ["one", "two"],
+  };
 
-  function openModal() {
-    setIsOpen(true);
-  }
-  const [query, setQuery] = useState(" ");
-  const searchList = [
-    {
-      id: 1,
-      name: "What is HashRaft ?",
-    },
-    {
-      id: 2,
-      name: "Our Services",
-    },
-    {
-      id: 3,
-      name: "Our Team",
-    },
-    {
-      id: 4,
-      name: "Our Clients",
-    },
-    {
-      id: 5,
-      name: "Our Partners",
-    },
-    {
-      id: 6,
-      name: "Our Blog",
-    },
-    {
-      id: 7,
-      name: "Our Contact",
-    },
-  ];
-  const filteredsearchList = searchList.filter((item) =>
-    item.name.toLowerCase().includes(query.toLowerCase())
+  const filteredSearchList = Object.keys(searchList).filter((key) =>
+    key.toLowerCase().includes(query.toLowerCase())
   );
+console.log(filteredSearchList,'some')
 
+  const handleOpenModal = () => {
+    setIsOpen(true);
+    setSelectedOption(null);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+    setSelectedOption(null);
+  };
+
+  const handleSearchListSelection = (key) => {
+    setSelectedOption(searchList[key]);
+    setQuery(key);  
+    setBackgroundColor("bg-blue-300 flex-none w-min font-mono rounded px-2");
+  };
+
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value);
+    setSelectedOption(null);
+  };
+
+  console.log(selectedOption, "selectedOption");
   return (
     <>
       <div>
         <button
           className="flex items-center xl:text-sm text-lg xl:text-slate-400 text-slate-800 dark:text-slate-300 px-1 space-x-3 rtl:space-x-reverse"
-          onClick={openModal}
+          onClick={handleOpenModal}
         >
           <>
             <Icon icon="heroicons-outline:search" />
@@ -64,7 +61,7 @@ const SearchModal = () => {
         <Dialog
           as="div"
           className="fixed inset-0 z-[9999] overflow-y-auto p-4 md:pt-[25vh] pt-20"
-          onClose={closeModal}
+          onClose={handleCloseModal}
         >
           <Transition.Child
             as={Fragment}
@@ -91,49 +88,50 @@ const SearchModal = () => {
               <Combobox>
                 <div className="relative">
                   <div className="relative mx-auto max-w-xl rounded-md bg-white dark:bg-slate-800 shadow-2xl ring-1 ring-gray-500-500 dark:ring-light divide-y divide-gray-500-300 dark:divide-light">
-                    <div className="flex bg-white dark:bg-slate-800  px-3 rounded-md py-3 items-center">
+                    <div className="flex bg-white dark:bg-slate-800 gap-2  px-3 rounded-md py-3 items-center">
                       <div className="flex-0  text-slate-700 dark:text-slate-300 ltr:pr-2 rtl:pl-2 text-lg">
                         <Icon icon="heroicons-outline:search" />
                       </div>
                       <Combobox.Input
-                        className="bg-transparent outline-none focus:outline-none border-none w-full flex-1 dark:placeholder:text-slate-300 dark:text-slate-200"
+                        value={query}
+                        className={`bg-transparent ${backgroundColor} outline-none  focus:outline-none border-none w-full flex-1 dark:placeholder:text-slate-300 dark:text-slate-200 `}
                         placeholder="Search..."
-                        onChange={(event) => setQuery(event.target.value)}
+                        onChange={handleQueryChange}
                       />
                     </div>
-                    <Transition
-                      leave="transition ease-in duration-100"
-                      leaveFrom="opacity-100"
-                      leaveTo="opacity-0"
-                    >
-                      <Combobox.Options className="max-h-40 overflow-y-auto text-sm py-2">
-                        {filteredsearchList.length === 0 && query !== "" && (
-                          <div>
-                            <div className=" text-base py-2 px-4">
-                              <p className="text-slate-500 text-base dark:text-white">
-                                No result found
-                              </p>
+                    <Combobox.Options className="max-h-40 overflow-y-auto text-sm py-2">
+                      {filteredSearchList.map((key, i) => (
+                        <Combobox.Option key={i}>
+                          {({ active }) => (
+                            <div
+                              className={`px-4 text-[15px] font-normal capitalize py-2 hover:bg-slate-900 hover:text-white duration-500 border-b border-gray-100 ${
+                                active
+                                  ? " dark:bg-slate-600 dark:bg-opacity-60 "
+                                  : "text-slate-900 dark:text-white"
+                              }`}
+                              onClick={() => handleSearchListSelection(key)}
+                            >
+                              <span className="cursor-pointer font-mono ">
+                                {key}:
+                              </span>
                             </div>
+                          )}
+                        </Combobox.Option>
+                      ))}
+                    </Combobox.Options>
+                    {selectedOption && (
+                      <div>
+                        {selectedOption.map((value, i) => (
+                          <div
+                            key={i}
+                            onClick={() => handleSearchListSelection(value)}
+                            className="px-4 cursor-pointer py-1 hover:bg-slate-900 hover:text-white border-b border-gray-100  duration-500"
+                          >
+                            {value}
                           </div>
-                        )}
-
-                        {filteredsearchList.map((item, i) => (
-                          <Combobox.Option key={i}>
-                            {({ active }) => (
-                              <div
-                                className={`px-4 text-[15px] font-normal capitalize py-2 ${
-                                  active
-                                    ? "bg-slate-900 dark:bg-slate-600 dark:bg-opacity-60 text-white"
-                                    : "text-slate-900 dark:text-white"
-                                }`}
-                              >
-                                <span>{item.name}</span>
-                              </div>
-                            )}
-                          </Combobox.Option>
                         ))}
-                      </Combobox.Options>
-                    </Transition>
+                      </div>
+                    )}
                   </div>
                 </div>
               </Combobox>
